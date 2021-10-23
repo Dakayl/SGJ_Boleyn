e6b859888117291fd0299c9928807460aa30f102
leann <leann.farant@outlook.fr>
2021-10-23 15:26:09 +0200

    fonction ajout de carte

--- ReignBoleyn/Assets/Scripts/Deck.cs
+++ ReignBoleyn/Assets/Scripts/Deck.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class Deck:MonoBehaviour
 {
     [SerializeField] private List<ChoiceCard> currentDeck;
@@ -12,17 +13,21 @@ public class Deck:MonoBehaviour
    }
 
    //Ajouter une carte au début
-   public void addCardAtEnd(ChoiceCard card) {
+   public void addCardAtFirst(ChoiceCard card) {
+       currentDeck.Insert(0, card);
    }
 
    //Ajouter une carte aléatoirement
    public void addCardAtRandom(ChoiceCard card) {
+       int addCardAtRandom = Random.Range(0, currentDeck.Count);
+       currentDeck.Insert(addCardAtRandom, card);
    }
 
    //Prendre la première carte
    public ChoiceCard getFirstCard() {
+       ChoiceCard card = currentDeck[0];
+       currentDeck.Remove(card);
+       return card;
        
    }
-
-
 }
