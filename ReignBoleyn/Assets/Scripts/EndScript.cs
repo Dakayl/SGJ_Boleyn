9adde30fb1024208a5ca84b1147af84f6810579f
Nicolas B <nicolas.brouet84@gmail.com>
2021-10-24 10:48:22 +0200

    fix metas

--- ReignBoleyn/Assets/Scripts/EndScript.cs
+++ ReignBoleyn/Assets/Scripts/EndScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndScript : MonoBehaviour
 {
-    private string titre;
-    private string texte;
+    private Canvas canvas;
+    private TextMeshProUGUI description;
+    private TextMeshProUGUI titre;
     [SerializeField] private Sprite imageFantasme;
     [SerializeField] private Sprite imageMere;
     [SerializeField] private Sprite imageVictime;
@@ -18,23 +20,34 @@ public class EndScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        canvas = transform.Find("Canvas").GetComponent<Canvas>();
+        titre = canvas.transform.Find("Titre").GetComponent<TextMeshProUGUI>();
+        description = canvas.transform.Find("Description").GetComponent<TextMeshProUGUI>();
         switch(GlobalParameters.ending){
             case "fantasme":
+                titre.SetText("La Fantasmée");
                 break;
             case "mere":
+                titre.SetText("La Mère");
                 break;
             case "victime":
+                titre.SetText("La Victime");
                 break;
             case "reine":
+                titre.SetText("La Victime");
                 break;
             case "sainte":
+                titre.SetText("La Sainte");
                 break;
             case "genereuse":
+                titre.SetText("La Généreuse");
                 break;
             case "lambda":
+                titre.SetText("La reine lambda");
                 break;
             default:
             case "putain":
+                titre.SetText("La Putain");
                 break;
          
     }
